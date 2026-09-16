d3be3dae1fdfc4740ce8bdf8fa2fab149360b83f
Andrejs Belokurovs <andrejs.belokurovs91@gmail.com>
2026-09-16 18:55:25 +0300

    feat: add theme checkbox and logic

---
+++ script.js
@@ -0,0 +1,23 @@
+const themeToggle = document.getElementById('toggle-theme');
+const body = document.body;
+
+themeToggle.addEventListener('click', function (e) {
+  e.target.checked ? toggleTheme('dark') : toggleTheme('light');
+});
+
+function toggleTheme(theme) {
+  localStorage.setItem('theme', theme);
+  body.setAttribute('data-theme', theme);
+}
+
+function updateTheme() {
+  localStorage.getItem('theme')
+    ? body.setAttribute('data-theme', localStorage.getItem('theme'))
+    : body.setAttribute('data-theme', 'light');
+
+  localStorage.getItem('theme') === 'dark'
+    ? (themeToggle.checked = true)
+    : (themeToggle.checked = false);
+}
+
+window.addEventListener('DOMContentLoaded', updateTheme);
