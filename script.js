bdd296cc867f69be04d165ecab242cd4dbfebd6e
Andrejs Belokurovs <andrejs.belokurovs91@gmail.com>
2026-09-18 17:09:30 +0300

    feat: add header responsive

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-const themeToggle = document.querySelector('.toggle');
-const body = document.body;
+window.addEventListener('DOMContentLoaded', function () {
+  const themeToggle = document.querySelector('.toggle');
+  const body = document.body;
 
-themeToggle.addEventListener('click', function (e) {
-  e.target.value === 'dark' ? toggleTheme('dark') : toggleTheme('light');
+  updateTheme();
+
+  themeToggle.addEventListener('click', function (e) {
+    e.target.value === 'dark' ? toggleTheme('dark') : toggleTheme('light');
+  });
+
+  function toggleTheme(theme) {
+    localStorage.setItem('theme', theme);
+    body.setAttribute('data-theme', theme);
+  }
+
+  function updateTheme() {
+    localStorage.getItem('theme')
+      ? body.setAttribute('data-theme', localStorage.getItem('theme'))
+      : body.setAttribute('data-theme', 'light');
+
+    localStorage.getItem('theme') === 'dark'
+      ? (themeToggle.checked = true)
+      : (themeToggle.checked = false);
+  }
 });
-
-function toggleTheme(theme) {
-  localStorage.setItem('theme', theme);
-  body.setAttribute('data-theme', theme);
-}
-
-function updateTheme() {
-  localStorage.getItem('theme')
-    ? body.setAttribute('data-theme', localStorage.getItem('theme'))
-    : body.setAttribute('data-theme', 'light');
-
-  localStorage.getItem('theme') === 'dark'
-    ? (themeToggle.checked = true)
-    : (themeToggle.checked = false);
-}
-
-window.addEventListener('DOMContentLoaded', updateTheme);
-
-// const themeToggle = document.getElementById('toggle-theme');
-// const body = document.body;
-
-// themeToggle.addEventListener('click', function (e) {
-//   e.target.checked ? toggleTheme('dark') : toggleTheme('light');
-// });
-
-// function toggleTheme(theme) {
-//   localStorage.setItem('theme', theme);
-//   body.setAttribute('data-theme', theme);
-// }
-
-// function updateTheme() {
-//   localStorage.getItem('theme')
-//     ? body.setAttribute('data-theme', localStorage.getItem('theme'))
-//     : body.setAttribute('data-theme', 'light');
-
-//   localStorage.getItem('theme') === 'dark'
-//     ? (themeToggle.checked = true)
-//     : (themeToggle.checked = false);
-// }
-
-// window.addEventListener('DOMContentLoaded', updateTheme);
